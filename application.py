269c082f0829b55687c85c01b3e72a448072b58d
RDallavia <rcd_1693@pm.me>
2024-01-25 21:07:44 -0600

    first site commit

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 application = Flask(__name__)
 
 @application.route('/')
-def hello_world():
-    return "Hello There! Good sir!"
+def index():
+    return render_template("index.html")
