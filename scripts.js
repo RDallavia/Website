a5c4524b202c9164cf9e5a6f8ac1e4e0aad77d37
RDallavia <rcd_1693@pm.me>
2024-01-29 20:43:38 -0600

    new

--- scripts.js
+++ scripts.js
@@ -1,4 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
+const timelineData = [
+    {
+        date: 'Jun 2022 - Present',
+        title: 'Springboard | Data Science Fellow',
+        description: '•	Successfully completed over 550 hours of training in all aspects of the data science workflow.'
+        '•	Peer-mentor to students new to the program and less experienced in programming and data science.',
+        '•	Executed the data science and machine learning projects listed at the top of the CV.'
+        
+    },
+    {
+        date: 'Aug 2019 - Jan 2022',
+        title: 'Freelance Sarcasm Specialist',
+        description: 'Offered a wide range of sarcasm consulting services. Helped clients understand the fine line between a hilarious email and an HR issue.'
+    },
+    {
+        date: 'May 2017 - Jul 2019',
+        title: 'Amateur Coffee Brewer',
+        description: 'Worked at JavaGenius, a tech startup where the code was less important than the coffee. Received the "Caffeine Innovator" award for my cold brew technique that kept coders awake for 72 hours straight.'
+    }
+];
+
     const timeline = document.getElementById('timeline');
 
     timelineData.forEach((item, index) => {
